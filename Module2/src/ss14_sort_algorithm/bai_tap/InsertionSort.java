17c5f0d858c6e0809a4dd2bd6965b7b988e901eb
khanhnkq <khanhnkq.24itb@vku.udn.vn>
2025-10-01 16:22:54 +0700

    update

--- Module2/src/ss14_sort_algorithm/bai_tap/InsertionSort.java
+++ Module2/src/ss14_sort_algorithm/bai_tap/InsertionSort.java
@@ -4,12 +4,13 @@ public class InsertionSort {
     public static void insertionSort(int[] list) {
 
         for (int i = 0; i < list.length; i++) {
-            int key = list[i];
+            int key = list[i];// phần tử cần chèn
             int j = i - 1;
-            while (j >= 0 && list[j] > key) {
+            while (j >= 0 && list[j] > key) {// Dịch các phần tử lớn hơn key sang phải
                 list[j + 1] = list[j];
                 j = j - 1;
             }
+            // Chèn key vào vị trí đúng
             list[j + 1] = key;
         }
     }
